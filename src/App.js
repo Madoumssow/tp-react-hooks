9cd3be92f2e7d4f9e7d219af23063de012424601
Thierno Mamaoudou Sow <smadoumssow33@gmail.com>
2024-11-04 14:30:26 +0000

    Les hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import sow from './images/sow.JPG'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      Personne: {
+        fullName: "Thierno Mamoudou Sow",
+        bio: "Développeur passionné par la technologie et l'inovation",
+        imgSrc: sow,
+        profession: "Développeur Back-End"
+      },
+      show: false,
+      elapsedTime: 0
+    }
+    this.toggleShow = this.toggleShow.bind(this)
+  }
+  toggleShow() {
+    this.setState((prevState) => ({
+      show: !prevState.show
+    }))
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.setState((prevState) => ({
+        elapsedTime: prevState.elapsedTime + 1
+      }))
+    }, 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+  
+  render() {
+    const { Personne, show, elapsedTime } = this.state
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className='App'>
+        <h1>Profile App</h1>
+        <button onClick={this.toggleShow}>
+          {show ? "Masquer le Profil": "Afficher le Profil"}
+        </button>
+          {show && (
+            <div className="profile">
+              <img src={Personne.imgSrc} alt={Personne.fullName} />
+              <h2>{Personne.fullName}</h2>
+              <p>{Personne.bio}</p>
+              <h3>Profession : {Personne.profession}</h3>
+            </div>
+          )}
+
+          <p>Temps écoulé depuis le montage : {elapsedTime} secondes</p>
+      </div>
+    )
+  }
 }
 
 export default App;
